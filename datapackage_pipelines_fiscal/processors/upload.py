40545c74ee55ba67d41c59f796a00896f1543083
Adam Kariv <adam.kariv@gmail.com>
2016-12-14 12:59:02 +0200

    Add row count to datapackage before upload

--- datapackage_pipelines_fiscal/processors/upload.py
+++ datapackage_pipelines_fiscal/processors/upload.py
@@ -1,3 +1,4 @@
+import json
 import os
 import zipfile
 import tempfile
@@ -7,7 +8,21 @@ from datapackage_pipelines.wrapper import ingest, spew
 import gobble
 
 params, datapackage, res_iter = ingest()
-spew(datapackage, res_iter)
+
+line_count = 0
+
+
+def line_counter(_res_iter):
+    def process_rows(_rows):
+        global line_count
+        for row in _rows:
+            line_count += 1
+            yield row
+
+    for res in _res_iter:
+        yield process_rows(res)
+
+spew(datapackage, line_counter(res_iter))
 
 user = gobble.user.User()
 in_filename = open(params['in-file'], 'rb')
@@ -18,6 +33,9 @@ for name in in_file.namelist():
     in_file.extract(name, temp_dir)
 in_file.close()
 datapackage_json = os.path.join(temp_dir, 'datapackage.json')
+datapackage = json.load(open(datapackage_json))
+datapackage['count_of_rows'] = line_count
+json.dump(datapackage, open(datapackage_json, 'w'))
 
 package = gobble.fiscal.FiscalDataPackage(datapackage_json, user=user)
 package.upload(skip_validation=True, publish=params.get('publish', False))
