5f7607a4f5ad4cefab4e437a77419b4d09b90450
Brook Elgie <specialbrew@gmail.com>
2018-07-20 14:30:37 +0100

    Better handle hyphens and underscores in datapackage ids
    Slugify with '_' as a separator, while allowing '-' and '.' characters
    in the datapackage id.

--- datapackage_pipelines_fiscal/flows/utils.py
+++ datapackage_pipelines_fiscal/flows/utils.py
@@ -1,18 +1,24 @@
-from datapackage_pipelines.generators import slugify
 from hashlib import md5
+
+from slugify import Slugify
+
+
+slugger = Slugify(separator="_", safe_chars="-.")
+
 
 def extract_names(source):
     title = source['title']
     dataset_name = source.get('dataset-name', title)
-    dataset_name = slugify(dataset_name, separator='_').lower()
+    dataset_name = slugger(dataset_name).lower()
     resource_name = source.get('resource-name', dataset_name)
 
     return title, dataset_name, resource_name
 
+
 def extract_storage_ids(source):
     owner_id = source['owner-id']
     _, dataset_name, _ = extract_names(source)
-    dataset_id = '{}:{}'.format(owner_id, dataset_name.replace('_', '-'))
+    dataset_id = '{}:{}'.format(owner_id, dataset_name)
     dataset_path = '{}/{}'.format(owner_id, dataset_name)
     dataset_name_hash = md5(dataset_name.encode('ascii')).hexdigest()[:16]
     db_table = '{}{}'.format(owner_id, dataset_name_hash)
